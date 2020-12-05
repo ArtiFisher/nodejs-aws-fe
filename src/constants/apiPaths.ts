666de7bd31ad0853bfd7e178eb8f3a866588561e
ArtiFisher <artemis.fisher@gmail.com>
2020-12-05 17:41:14 +0300

    feat: Add path to cart API

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -4,7 +4,7 @@ const API_PATHS = {
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   import: 'https://aqnw2royh7.execute-api.eu-west-1.amazonaws.com/dev',
   bff: 'https://g86uhrmyl7.execute-api.eu-west-1.amazonaws.com/dev',
-  cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+  cart: 'http://artifisher-cart-api-dev.eu-west-1.elasticbeanstalk.com',
 };
 
 export default API_PATHS;
